4f93ff45df1dc1899744d3016bb7109e87032734
Katrina Wheelan <kwheelan@detroitmi.gov>
2024-12-08 20:12:02 -0500

    #7 script works

--- scripts/alter_formulas.py
+++ scripts/alter_formulas.py
@@ -23,14 +23,24 @@ class Replacer:
     
     def replace_function(self, formula):
         # Regular expression to match the pattern
-        pattern = rf"('{self.sheet}'!\$){self.old_col}(.+:\$){self.old_col}(.+)"
-
-        # Replacement pattern with backreferences
-        replacement = rf"\1{self.new_col}\2{self.new_col}\3"
+        pattern = rf"('{self.sheet}'!\$){self.old_col}(\$[\d]+)?(:\$)?({self.old_col})?(\$[\d]+)?"
+        
+        def repl(match):
+            sheet_part = match.group(1)
+            first_reference_part = match.group(2) or ''
+            range_separator = match.group(3) or ''
+            second_col_part = match.group(4) or ''
+            second_reference_part = match.group(5) or ''
+            
+            if range_separator:
+                # If range_separator is present, handle whole column and regular ranges
+                return f"{sheet_part}{self.new_col}{first_reference_part}{range_separator}{self.new_col}{second_reference_part}"
+            else:
+                # Single cell or whole column reference
+                return f"{sheet_part}{self.new_col}{first_reference_part}"
 
         # Replace the pattern in the text
-        result = re.sub(pattern, replacement, formula)
-
+        result = re.sub(pattern, repl, formula)
         return(result) 
 
 #======================================================================
@@ -42,9 +52,19 @@ SOURCE_FOLDER = 'C:/Users/katrina.wheelan/OneDrive - City of Detroit/Documents -
 REPLACEMENTS = [
     Replacer('FTE, Salary-Wage, & Benefits', 'AG', 'AR'),
     Replacer('FTE, Salary-Wage, & Benefits', 'U', 'AO'),
+    Replacer('FTE, Salary-Wage, & Benefits', 'AA', 'AP'),
+    Replacer('FTE, Salary-Wage, & Benefits', 'AF', 'AQ'),
+    Replacer('Overtime & Other Personnel', 'R', 'Y'),
+    Replacer('Overtime & Other Personnel', 'V', 'Z'),
     Replacer('Overtime & Other Personnel', 'W', 'AC'),
     Replacer('Non-Personnel', 'Y', 'AC')
 ]
+
+POLICE_REPLACEMENTS = REPLACEMENTS.copy()
+POLICE_REPLACEMENTS[0] = Replacer('FTE, Salary-Wage, & Benefits', 'AH', 'AS')
+POLICE_REPLACEMENTS[1] = Replacer('FTE, Salary-Wage, & Benefits', 'V', 'AP')
+POLICE_REPLACEMENTS[2] = Replacer('FTE, Salary-Wage, & Benefits', 'AB', 'AQ')
+POLICE_REPLACEMENTS[3] = Replacer('FTE, Salary-Wage, & Benefits', 'AG', 'AR')
 
 SHEETS = ['Dept Summary', 'Initiatives Summary']
 
@@ -52,8 +72,12 @@ SHEETS = ['Dept Summary', 'Initiatives Summary']
 # Helpers
 #======================================================================
 
-def replace_all(formula):
-    for replacer in REPLACEMENTS:
+def replace_all(formula, police = False):
+    if police:
+        replacements = POLICE_REPLACEMENTS
+    else: 
+        replacements = REPLACEMENTS
+    for replacer in replacements:
         formula = replacer.replace_function(formula)
     return formula
 
@@ -94,18 +118,23 @@ def create_file_list(verbose = False):
     return DS_list
 
 def edit_formulas(file, verbose=False):
+    # check if it's the police DS
+    police = 'Police' in file
+
     # open file 
     wb = load_workbook(file, data_only=False)
+
+    # make replacements on both dept summary and init summary
     for sheet in SHEETS:
         ws = wb[sheet]
         
         # Iterate through all cells in the sheet
         for row in ws.iter_rows():
             for cell in row:
-                if isinstance(cell.value, str) and cell.value.startswith('=') and 'SUMIFS' in cell.value:
+                if isinstance(cell.value, str) and cell.value.startswith('='): # and 'SUMIFS' in cell.value:
                     # replace formula
                     original_formula = cell.value
-                    new_formula = replace_all(original_formula)
+                    new_formula = replace_all(original_formula, police)
                     if original_formula != new_formula:
                         # print optional message
                         if verbose:
@@ -115,17 +144,25 @@ def edit_formulas(file, verbose=False):
     # save workbook
     name = file.split('\\')[-1]
     wb.save(f'output/formula_replacements/{name}')
-    print(f'saved as {name}')
+    print(f'Saved {name}')
 
 #======================================================================
 # Main
 #======================================================================
 
+def test():
+    #text = """'FTE, Salary-Wage, & Benefits'!$AG$2 + SUM('FTE, Salary-Wage, & Benefits'!$AG$15:$AG$1000)"""
+    text = """=SUMIFS('FTE, Salary-Wage, & Benefits'!$AG:$AG,'FTE, Salary-Wage, & Benefits'!$L:$L,"Baseline",'FTE, Salary-Wage, & Benefits'!$G:$G,$W7)
++SUMIFS('Overtime & Other Personnel'!$W:$W,'Overtime & Other Personnel'!$N:$N,"Baseline",'Overtime & Other Personnel'!$G:$G,$W7)
++SUMIFS('Non-Personnel'!$Y:$Y,'Non-Personnel'!$O:$O,"Baseline*",'Non-Personnel'!$G:$G,$W7)
+"""
+    print(replace_all(text))
+
 def main():
     files = create_file_list()
-    for file in files[0:1]:
-        #print(file)
+    for file in files:
         edit_formulas(file)
 
 if __name__ == '__main__':
+    #test()
     main()

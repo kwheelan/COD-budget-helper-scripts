cc5c370132deb8a32fecaecdfce1060fc7bc50c1
Katrina Wheelan <kwheelan@detroitmi.gov>
2025-02-16 16:18:54 -0500

    add custom cols for FTE tables

--- scripts/budget_book/models/SectionA/fteTable.py
+++ scripts/budget_book/models/SectionA/fteTable.py
@@ -13,6 +13,10 @@ class FteTable(ExcelTable):
         # main_header = "FY2025 - FY2029 Budgeted Positions by Department"
         super().__init__(custom_df)
 
+    def column_format(self, format=None):
+        n_cols = len(self.table_data().columns)
+        return r'|>{\centering\arraybackslash}p{2.5cm}|' + 'p{0.5cm}|' + 'p{2.75cm}|' + r'>{\centering\arraybackslash}p{1.25cm}|' * (n_cols - 3)
+
     def process_latex(self):
         # self.rename_cols()
         self.latex = self.default_latex()
